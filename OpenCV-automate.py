a84e22aaa9689fee8028c3e6c011ea9fa00ba30a
Arihant Jain <14129747+arihantjain124@users.noreply.github.com>
2020-06-11 01:36:47 +0530

    Improved Logic and Accuracy

--- OpenCV-automate.py
+++ OpenCV-automate.py
@@ -5,24 +5,47 @@ client = AdbClient(host="127.0.0.1", port=5037)
 devices = client.devices()
 device = devices[0]
 # dev = client.device(device[0])
+x = 450
+black = [0, 0, 0]
 while (True):
+    p1 = 0
+    p2 = 0
+    p3 = 0
+    p4 = 0
+    flag = 0
     s = "input touchscreen swipe 170 187 170 187"  # + delay in milliseconds
     result = device.screencap()
-    with open("screen.png", "wb") as fp:
+    with open("image.png", "wb") as fp:
         fp.write(result)
-    img = cv2.imread("screen.png")
-    imgh = int(img.shape[0]) * 3 // 4
-    x = int(img.shape[0] / 120)
-    imgw = int(img.shape[1] / 4.32)
-    img = img[imgh + x - 1:imgh + x, imgw:]
-    line = img.shape[1]
-    for i in range(line):
-        if img[0][i][2] > 200 and img[0][i][1] < 50 and img[0][i][0] < 50:
-            break
-    if i < 800:
-        s = s+" "+str(i)
-        device.shell(s)
-        print(i)
-    # cv2.imshow("img", img)
-    # cv2.waitKey(1)
-    time.sleep(4)
+    img = cv2.imread("image.png")
+    imgw = int(img.shape[1])
+    imgh = int(img.shape[0])
+    img = img[imgh - x - 1:imgh - x, :]
+    img = img.reshape(imgw, 3)
+    line = [list(i) for i in img]
+    pixels = (len(line))
+    print(pixels)
+    for i in range(pixels):
+        if line[i] == black and flag == 0:
+            if p1 == 0:
+                p1 = i
+            else:
+                p3 = i
+            flag = 1
+            continue
+        if line[i] != black and flag == 1:
+            if p2 == 0:
+                p2 = i
+            else:
+                p4 = i
+            flag = 0
+            continue
+    print(p1, p2, p3, p4)
+    distance = p3 - p2
+    red = (p4 - p3) / 2 + distance
+    print(red)
+    i = int(red*0.98)
+    s = s+" "+str(i)
+    device.shell(s)
+    print(i)
+    time.sleep(3.5)
